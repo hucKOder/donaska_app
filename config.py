b2f986e08535872f54e3934a877e49add0eb26e4
xhuckoa1 <hucko.andrej.mail@gmail.com>
2020-03-18 15:18:52 +0100

    Setup login form user model and form validation

--- config.py
+++ config.py
@@ -2,6 +2,6 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'sqlite:///' + os.path.join(basedir, 'app.db')
+    SECRET_KEY = 'pre-dobru-vec'
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
     SQLALCHEMY_TRACK_MODIFICATIONS = False
